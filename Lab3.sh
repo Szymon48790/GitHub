48e5547966aeaad5001482a1569e2f89cbd35b0d
Szymon48790 <131284687+Szymon48790@users.noreply.github.com>
2023-04-21 15:01:19 +0200

    Add files via upload

---
+++ Lab3.sh
@@ -0,0 +1,63 @@
+#zad1
+#git commit
+#git commit
+#zad2
+#git branch
+#git checkout -b bugFix
+#zad3
+#git checkout -b bugFix
+#git commit
+#git checkout main
+#git commit
+#git merge bugFix
+#zad4
+#git checkout -b bugFix
+#git commit
+#git checkout main
+#git commit
+#git checkout bugFix
+#git rebase main
+#zad5
+#git checkout C4
+#zad6
+#git checkout bugFix^
+#zad7
+#git checkout bugFix
+#git branch -f bugFix HEAD~3
+#git checkout C6
+#git checkout -f HEAD~3
+#git branch -f main C6
+#zad8
+#git reset local
+#git reset local~1
+#git checkout pushed
+#git revert pushed
+#zad9
+#git cherry-pick C3
+#git cherry-pick C4
+#git cherry-pick C7
+#zad10
+#git reabse -i HEAD~4
+#zad11
+#git checkout main
+#git cherry-pick C4
+#zad12
+#git rebase -i HEAD~2
+#git commit --amend
+#git rebase -i HEAD~2
+#git checkout main
+#git merge caption
+#zad13
+#git checkout main
+#git cherry-pick C2
+#git commit --amend
+#git cherry-pick C3
+#zad14
+#git checkout C1
+#git tag v0 C1
+#git checkout C2
+#git tag v1 C2
+#zad15
+#git describe mine
+#git describe side
+#git commit
